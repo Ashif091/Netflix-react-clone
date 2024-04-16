db7da6b85b0e6c1f1735e7803e91fc2d3912f65c
Ashif091 <fhyvhh091@gmail.com>
2024-04-16 09:23:04 +0530

    movie row updated

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,17 @@
 import Loader from "../components/Loader"
+import Row from "../components/Row"
+import requests from "../Requests"
+
+
 const Home = () => {
   return (
     <>
-    <Loader/>
-    
+     <Loader/>
+    <Row  rowId='1'  title='UpComing' fetchURL={requests.requestUpcoming}></Row>
+    <Row  rowId='2' title='Popular' fetchURL={requests.requestPopular}></Row>
+    <Row  rowId='3' title='Horror' fetchURL={requests.requestHorror}></Row>
+    <Row  rowId='4' title='TopRated' fetchURL={requests.requestTopRated}></Row>
+    <Row  rowId='5' title='Trending' fetchURL={requests.requestTrending}></Row>
     </>
   )
 }
